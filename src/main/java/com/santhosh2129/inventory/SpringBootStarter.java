8d46b51ecd8414531f89ace8e67bb5f4e6df4c24
Santhosh <sthiyagarajan@owler.com>
2017-07-20 17:46:58 +0530

    chnages

--- src/main/java/com/santhosh2129/inventory/SpringBootStarter.java
+++ src/main/java/com/santhosh2129/inventory/SpringBootStarter.java
@@ -2,12 +2,16 @@ package com.santhosh2129.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created by santhosh on 7/1/17.
  */
 
 @SpringBootApplication
+@EnableSwagger2
+@EnableDiscoveryClient
 public class SpringBootStarter {
     public static void main(String[] arg) {
         SpringApplication.run(SpringBootStarter.class, arg);
